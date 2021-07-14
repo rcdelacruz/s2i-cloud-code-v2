3349c6a1a7689d9ad8c93bf083821e9471b4efac
Ronald DC <apps@thebrewery.dev>
2021-07-14 21:56:44 +0800

    Update node to v14

--- root/opt/app-root/etc/post-script.sh
+++ root/opt/app-root/etc/post-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 #sed -i '1s/^/require(".\/send-mail");\n/' /opt/app-root/src/cloud/main.js
-cp -r /opt/app-root/etc/parse/src/cloud/. /opt/app-root/src/cloud
+#cp -r /opt/app-root/etc/parse/src/cloud/. /opt/app-root/src/cloud
 cp /opt/app-root/etc/parse/src/parse-config.js /opt/app-root/src/parse-config.js
+cp /opt/app-root/etc/parse/src/index.js /opt/app-root/src/index.js
